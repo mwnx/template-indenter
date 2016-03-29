98d637b2c4192a98369579a13c8c10f5706f2540
mwnx <mwnx@gmx.com>
2016-03-29 02:58:20 +0200

    Version 0

--- template-indenter.cpp
+++ template-indenter.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 #include <cstring>
+#include <regex>
 
 using std::ostream;
 using std::istream;
 using std::string;
 
+string colour_start_seq {"2"};
+string colour_end_seq {"22"};
+
+ostream& start_colour(ostream& os)
+{
+	return os << "[" << colour_start_seq << "m";
+}
+
+ostream& end_colour(ostream& os)
+{
+	return os << "[" << colour_end_seq << "m";
+}
+
 ostream& print_indent(ostream& os, int level, const string& seq)
 {
 	for (int i = 0; i < level; ++i) os << seq;
 	return os;
+}
+
+void skip_spaces(istream& is)
+{
+	while (is.good() && is.get() == ' ');
+	is.unget();
 }
 
 /* t_indent: indent template. */
@@ -20,7 +40,10 @@ ostream& t_indent(istream& is, ostream& os, int level, const string& indent)
 		switch (c) {
 		case '<':
 			os << '<' << std::endl;
+			start_colour(os);
 			t_indent(is, os, level + 1, indent);
+			if (level == 0) end_colour(os);
+			skip_spaces(is);
 			break;
 		case '>':
 			os << '>';
@@ -28,10 +51,9 @@ ostream& t_indent(istream& is, ostream& os, int level, const string& indent)
 			break;
 		case ',':
 			os << ',' << std::endl;
+			if (level > 0) start_colour(os);
 			print_indent(os, level, indent);
-			break;
-		case ' ':
-			if (level == 0) os << ' ';
+			skip_spaces(is);
 			break;
 		default:
 			os << (char)c;
@@ -43,7 +65,25 @@ ostream& t_indent(istream& is, ostream& os, int level, const string& indent)
 
 ostream& t_indent(istream& is, ostream& os, const string& indent)
 {
-	return t_indent(is, os, 0, indent);
+	while (is) {
+		int ch = is.get();
+		if (ch == '\n' || ch == EOF) {
+			return os;
+		} else if (ch == ' ' || ch == '\t') {
+			os << (char)ch;
+			continue;
+		} else {
+			is.unget();
+		}
+		string word;
+		is >> word;
+		os << word;
+		if (std::regex_match(word,
+				     std::regex{".*(error|note):.*"})) {
+			return t_indent(is, os, 0, indent);
+		}
+	}
+	return os;
 }
 
 ostream& t_indent_all(istream& is, ostream& os, string indent)
